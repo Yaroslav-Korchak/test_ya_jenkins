9a0ccac35e01fe3a937cb39666ec09430b681259
Yaroslav-Korchak <yakorchak@mail.ru>
2024-08-06 10:47:23 +0300

    headles mode

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -2,13 +2,19 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def browser():
     # Инициализация драйвера Chrome с использованием webdriver-manager
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
     driver.quit()
 
